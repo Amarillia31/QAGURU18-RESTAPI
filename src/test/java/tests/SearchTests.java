6d0c9dc7db0217acb087f52f1026bef01b86f889
elena <elena.doronina@exactpro.com>
2022-07-03 16:49:27 +0400

    one more test added

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -4,7 +4,6 @@ import com.codeborne.selenide.CollectionCondition;
 import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
